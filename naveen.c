b1afb35cd89ffcd210c36ed727f39c15bfe64816
naveenpatrudu <143468122+naveenpatrudu@users.noreply.github.com>
2023-08-29 07:44:03 +0530

    Add files via upload

---
+++ naveen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int a,b;
+    printf("enter 2 values : \n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("A = %d is greater ",a);
+    }
+    else
+    {
+        printf("B = %d is greater",b);
+    }
+   }
