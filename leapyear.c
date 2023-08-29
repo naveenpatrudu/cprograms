3bb05d0d5a31a2487d3aba598432315d280e552c
naveenpatrudu <143468122+naveenpatrudu@users.noreply.github.com>
2023-08-29 23:54:42 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("enter a values : \n");
+    scanf("%d",&a);
+    if(a%4 == 0)
+    {
+        printf("ITS a leap year");
+    }
+   
+     else 
+    {
+        printf("not a leap");
+    }
+    }
