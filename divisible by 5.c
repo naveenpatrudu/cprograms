8e88e9b9c8604023dee4e8e545ff36decf8356a6
naveenpatrudu <143468122+naveenpatrudu@users.noreply.github.com>
2023-08-29 23:52:54 +0530

    Create divisible by 5.c

---
+++ divisible by 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("enter a values : \n");
+    scanf("%d",&a);
+    if(a%5 == 0)
+    {
+        printf("number is divisible by 5");
+    }
+   
+     else 
+    {
+        printf("number is not divisible");
+    }
+    }
+
